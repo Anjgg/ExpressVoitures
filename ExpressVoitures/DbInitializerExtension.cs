11ff8a5275d0bc69eabd5714863ec04630d19ad5
Julien LEROY <julien.leroy.work@gmail.com>
2025-03-05 23:47:00 +0100

    Add Seeding Data

---
+++ ExpressVoitures/DbInitializerExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace ExpressVoitures
+{
+    public static class DbInitializerExtension
+    {
+        public static IApplicationBuilder SeedDataBase(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData.Initialize(serviceScope.ServiceProvider);
+            }
+            return app;
+        }
+    }
+}
